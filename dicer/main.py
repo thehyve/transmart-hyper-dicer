456947413a1bd0d850068e99bae878b979b23a70
Gijs Kant <gijs@thehyve.nl>
2019-07-16 14:44:39 +0200

    Improve command line tool. Read variables from .env.

--- dicer/main.py
+++ dicer/main.py
@@ -1,11 +1,15 @@
 import logging.config
+import json
 import os
+import sys
+import traceback
 from pathlib import Path
 
 import click
+from dotenv import load_dotenv
 import yaml
+from transmart_loader.console import Console
 
-from dicer.config import logging_config
 from dicer.hyper_dicer import HyperDicer
 from dicer.keycloak_rest_client import KeycloakConfiguration
 from dicer.transmart_rest_client import TransmartConfiguration
@@ -25,32 +29,62 @@ def setup_logging(default_level=logging.INFO):
         logging.basicConfig(level=default_level)
 
 
+def read_env_variable(variable_name: str) -> str:
+    res = os.environ.get(variable_name)
+    if res is None or len(res) == 0:
+        logging.error(f'Please configure environment variable {variable_name}')
+        sys.exit(1)
+    return res
+
+
 def read_config() -> TransmartConfiguration:
     """
     Reads connnection configuration from environment variables.
     :return: Transmart configuration object
     """
     keycloak_config = KeycloakConfiguration(
-        oidc_server_url='{}/auth/realms/{}'.format(
-            os.environ.get('KEYCLOAK_SERVER_URL'), os.environ.get('KEYCLOAK_REALM')),
+        url='{}/auth/realms/{}'.format(
+            read_env_variable('KEYCLOAK_SERVER_URL'), read_env_variable('KEYCLOAK_REALM')),
         client_id=os.environ.get('KEYCLOAK_CLIENT_ID', 'transmart-client'),
-        offline_token=os.environ.get('OFFLINE_TOKEN')
+        offline_token=read_env_variable('OFFLINE_TOKEN')
     )
     config = TransmartConfiguration(
-        url=os.environ.get('TRANSMART_URL'),
+        url=read_env_variable('TRANSMART_URL'),
         keycloak_config=keycloak_config
     )
     return config
 
 
+def read_constraint_from_file(input_file: Path) -> dict:
+    with input_file.open() as json_file:
+        Console.info('Reading constraint from file {} ...'.format(input_file))
+        return json.load(json_file)
+
+
 @click.command()
 @click.argument('input_file', type=click.Path(dir_okay=False, exists=True, readable=True))
 @click.argument('output_dir', type=click.Path(file_okay=False, writable=True))
+@click.option('--debug', is_flag=True, help='Print more verbose messages')
 @click.version_option()
-def dicer(input_file: Path, output_dir: Path):
-    setup_logging()
-    hyper_dicer = HyperDicer(read_config(), Path(input_file), Path(output_dir))
-    hyper_dicer.run()
+def dicer(input_file: Path, output_dir: Path, debug: bool):
+    """Reads data from a TranSMART server using the constraint in INPUT_FILE (in JSON format)
+       and writes staging files for transmart-copy to the empty directory OUTPUT_DIR.
+       \f
+       :param input_file: Data constraint in JSON format
+       :param output_dir: Empty output directory where staging files are written
+    """
+    try:
+        load_dotenv()
+        setup_logging(logging.DEBUG if debug else logging.INFO)
+        Console.title('TranSMART Hyper Dicer')
+        hyper_dicer = HyperDicer(read_config())
+        constraint = read_constraint_from_file(Path(input_file))
+        hyper_dicer.copy_slice(constraint, Path(output_dir))
+    except Exception as e:
+        Console.error(e)
+        if debug:
+            traceback.print_exc()
+        sys.exit(1)
 
 
 def main():
--- tests/test_hyper_dicer.py
+++ tests/test_hyper_dicer.py
@@ -4,7 +4,6 @@
 """Tests for the hyper_dicer tool.
 """
 from os import path
-from pathlib import Path
 import pytest
 
 from dicer.hyper_dicer import HyperDicer
@@ -12,9 +11,8 @@ from dicer.hyper_dicer import HyperDicer
 
 @pytest.mark.usefixtures('mocked_config', 'mocked_responses')
 def test_hyper_dicer(tmp_path, mocked_config, mocked_responses):
-    hyper_dicer = HyperDicer(mocked_config,
-                             Path('./tests/test_query.json'), tmp_path)
-    hyper_dicer.run()
+    hyper_dicer = HyperDicer(mocked_config)
+    hyper_dicer.copy_slice({'type': 'true'}, tmp_path)
     target_path = tmp_path.as_posix()
     assert path.exists(target_path + '/i2b2metadata/i2b2_secure.tsv')
     assert path.exists(target_path + '/i2b2demodata/concept_dimension.tsv')
